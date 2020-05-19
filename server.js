d18bbed05e0abb5d102a4dadc49e620424eca62d
Joseph Ledbetter <josephb.ledbetter@gmail.com>
2020-05-19 17:14:01 -0400

    completed express call corrected incorrect app.get()

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 
-const app = express('/', (req, res) => res.send('api running'));
+const app = express();
 
-app.get();
+app.get('/', (req, res) => res.send('api running'));
 
 // Processing the environment variable on a specific PORT specified throughout the project, or default 5000
 const PORT = process.env.PORT || 5000;
